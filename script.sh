b05db35f4cae440e4050939f2e35990f27cdbed4
Saad <saadaas999@gmail.com>
2024-10-17 15:24:11 -0500

    fixed again

--- script.sh
+++ script.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
+
+# Start a loop to keep prompting until a valid option is selected
 while true; do
-echo "Hello, please choose 1 option"
-echo "(1) Type anything and it will show in the terminal"
-echo "(2) Play a random sound"
-echo "(3) Exit"
-read -p "Enter your choice: " option
- 
-if [ "$option" == "1" ]; then
-read -p "Type anything: " text
-echo $text
-exit 0
+    echo "Hello, please choose 1 option"
+    echo "(1) Type anything and it will show in the terminal"
+    echo "(2) Play a random sound"
+    echo "(3) Exit"
+    read -p "Enter your choice: " option
 
-elif $option == 2 then
-find /home/saadomar123/Script/sounds -type f | shuf -n 1 | xargs -I{} mpg123 {}
-exit 0
+    # Handle the user's choice using if statements
+    if [ "$option" == "1" ]; then
+        read -p "Type anything: " text
+        echo "$text"
+        
+    elif [ "$option" == "2" ]; then
+        # Play a random sound from the specified folder
+        find /home/saadomar123/Script/sounds -type f -name "*.mp3" | shuf -n 1 | xargs -I{} mpg123 {}
 
-elif $option == 3 then
+    elif [ "$option" == "3" ]; then
+        echo "Goodbye!"
+        exit 0
 
-	echo "Goodbye!"
-	exit
-else
-     echo "Invalid option. Please run the script again and choose 1, 2, or 3."
-     fi
+    else
+        echo "Invalid option. Please choose 1, 2, or 3."
+    fi
 done
+
