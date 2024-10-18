f1d80a6bc0a6611f1db3d6805209e23dbf5a91e1
Saad <saadaas999@gmail.com>
2024-10-18 14:44:16 -0500

    Removed unneccesary comments

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Start a loop to keep prompting until a valid option is selected
+
 while true; do
     echo "Hello, please choose 1 option"
     echo "(1) Type anything and it will show in the terminal"
@@ -8,13 +8,13 @@ while true; do
     echo "(3) Exit"
     read -p "Enter your choice: " option
 
-    # Handle the user's choice using if statements
+    
     if [ "$option" == "1" ]; then
         read -p "Type anything: " text
         echo "$text"
         
     elif [ "$option" == "2" ]; then
-        # Play a random sound from the specified folder
+        
         find /home/saadomar123/Script/sounds -type f -name "*.mp3" | shuf -n 1 | xargs -I{} mpg123 {}
 
     elif [ "$option" == "3" ]; then
